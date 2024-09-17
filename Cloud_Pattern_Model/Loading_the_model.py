f09b0420d461889f22a84ecff4e8b1f6a3c9bb71
Hasitha Mihiranga <119967987+hm7mihiranga@users.noreply.github.com>
2024-09-17 19:51:08 +0530

    Update Loading_the_model.py

--- Cloud_Pattern_Model/Loading_the_model.py
+++ Cloud_Pattern_Model/Loading_the_model.py
@@ -23,7 +23,7 @@ label_to_colormap = dict(zip(labels, colormaps))
 idx_to_colormap = dict(zip(indexes, colormaps))
 
 # Load your saved model
-model = load_model('cloud_segmentation_model.h5') # Model Link : https://drive.google.com/drive/folders/140FhfHNMYHgHPhKvp9RE5jMf9MFCPdFx?usp=sharing
+model = load_model('cloud_segmentation_model.h5')  # Model Link : https://drive.google.com/drive/folders/140FhfHNMYHgHPhKvp9RE5jMf9MFCPdFx?usp=sharing
 
 # Load and preprocess the input image
 def load_and_preprocess_image(image_path):
@@ -46,7 +46,7 @@ def decode_prediction(pred_mask):
     return decoded_mask
 
 # Path to your input image
-image_path = 'hasi.jpg'  # Change this to your actual image path
+image_path = 'images.jpeg'  # Change this to your actual image path
 
 # Predict the mask
 pred_mask = predict(image_path)
@@ -67,18 +67,6 @@ plt.figure(figsize=(8, 8))
 plt.imshow(decoded_mask, cmap=custom_cmap)
 plt.colorbar(ticks=indexes, label='Classes')
 plt.title("Decoded Segmentation Mask with Class Labels")
-plt.show()
-
-# Display class label, color, and colormap information
-for idx in indexes:
-    label = idx_to_label[idx]
-    color = idx_to_color[idx]
-    rgb_color = idx_to_rgb_color[idx]
-    colormap = idx_to_colormap[idx]
-    print(f"Class {idx}: {label}")
-    print(f" - Color: {color}")
-    print(f" - RGB Color: {rgb_color}")
-    print(f" - Colormap: {colormap}")
 
 
 # Assuming 'decoded_mask' is your predicted mask with the label index at each pixel
@@ -97,12 +85,15 @@ def find_most_frequent_labels(decoded_mask, idx_to_label, top_n=1):
 
     return frequent_label_names, most_frequent_labels
 
-# Decode the predicted mask if not already done
-# decoded_mask = decode_prediction(pred_mask)  # Uncomment if pred_mask is present
+# Find the most frequent label
+most_frequent_labels, _ = find_most_frequent_labels(decoded_mask, idx_to_label, top_n=1)
 
-# Find the top 2 most frequent labels (or change top_n as per your need)
-most_frequent_labels, label_frequencies = find_most_frequent_labels(decoded_mask, idx_to_label, top_n=2)
-
-# Display the result
-print(f"Most Frequent Label Names: {most_frequent_labels}")
-print(f"Label Frequencies: {label_frequencies}")
+# Print A, B, C, or D based on the most frequent label
+if most_frequent_labels[0] == 'Fish':
+    print("fair weather, but if the clouds thicken or lower, rain may be on the way")
+elif most_frequent_labels[0] == 'Gravel':
+    print("fair weather, but if it becomes more extensive or dense, it could suggest an impending change in weather to Rainy")
+elif most_frequent_labels[0] == 'Sugar':
+    print("There is high possibility to rain")
+elif most_frequent_labels[0] == 'Flower':
+    print("Can be Change Whether for Rainy Situation This suggest Fair whether")
